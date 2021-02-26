e72cadec4711fe6920176875e8fcc7243c491b89
Domingos Ribeiro <domingos.ribeiro@gmail.com>
2021-02-26 11:59:27 +0000

    Add project files.

---
+++ CalcMath.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class
+{
+    class CalcMath
+    {// Métodos para invocar na Class Form1
+
+        public int x; // o x é um value member da class calcMath, mas foi incorretamente especificado como Public
+        public int y; // o y é um value member da class calcMath, mas foi incorretamente especificado como Public
+
+        public int X
+        {
+            get
+            {
+                return x;
+            }
+
+            set
+            {
+                x = value;
+            }
+        }
+        public int Y
+        {
+            get
+            {
+                return y;
+            }
+
+            set
+            {
+                x = value;
+            }
+        }
+
+        public int Somar()
+        {
+            return x + y;
+        }
+
+        public int Multiplicar()
+        {
+            return x * y;
+        }
+
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Class
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btnSoma_Click(object sender, EventArgs e)
+        {
+            int n1 = Convert.ToInt32(txtNum1.Text);
+            int n2 = Convert.ToInt32(txtNum2.Text);
+
+            CalcMath obj = new CalcMath();
+
+            obj.x = n1;// o x é um value member da class calcMath, mas foi incorretamente especificado como Public
+            obj.y = n1;// o y é um value member da class calcMath, mas foi incorretamente especificado como Public
+            int resultado = obj.Somar();
+            txtResultado.Text = Convert.ToString(resultado);
+
+        }
+
+        private void btnMultiplica_Click(object sender, EventArgs e)
+        {
+            int n1 = Convert.ToInt32(txtNum1.Text);
+            int n2 = Convert.ToInt32(txtNum2.Text);
+
+            CalcMath obj = new CalcMath();
+
+            obj.x = n1;
+            obj.y = n1;
+            int resultado = obj.Multiplicar();
+            txtResultado.Text = Convert.ToString(resultado);
+        }
+
+
+    }
+}
